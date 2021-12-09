6b7462cc4b63a9a48eed3fab44d1cad980d269a4
erikajimenez <erika.jimenez@tsjcdmx.gob.mx>
2021-12-08 22:08:49 -0600

    correomasivo

--- app/Http/Controllers/EnviaCorreoController.php
+++ app/Http/Controllers/EnviaCorreoController.php
@@ -14,18 +14,21 @@ ini_set('memory_limit','20G');
 class EnviaCorreoController extends Controller
 {
     public function enviaCorreo(){
-        $info_usuarios = User::where('activo','=', 1)->whereIn('id_red',[2,4])->get();
+        $info_usuarios = User::where('activo','=', 0)->whereIn('id_red',[2,4])->get();
         $countUsuario=0;
         //echo $info_usuarios;
-        foreach($info_usuarios as $usuario){
-            $red = cat_redesconatrib::where('id','=', $usuario->id_red)->get();
+        /*foreach($info_usuarios as $usuario){*/
+            while ($countUsuario <= 90) {
+                # code...
+            
+            $red = cat_redesconatrib::where('id','=', $usuario[0]->id_red)->get();
             $info = [
-                'nombre' => $usuario->name,
-                'correo' => $usuario->email,
-                'password' => $usuario->password_plano,
+                'nombre' => $usuario[0]->name,
+                'correo' => $usuario[0]->email,
+                'password' => $usuario[0]->password_plano,
                 'red' => $red[0]->red,
                 ];
-            Mail::to($usuario->email)->send(new RegistroMail($info));
+            Mail::to($usuario[0]->email)->send(new RegistroMail($info));
             $countUsuario ++;
 
             //echo $usuario->email;
