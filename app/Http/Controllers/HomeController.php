15319211c0e424eb582052222c1dadb11c18b20a
Jair Pavon <jairo.p4195@gmail.com>
2022-11-13 11:00:03 -0600

    VistasLiberadas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -59,49 +59,49 @@ class HomeController extends Controller
 
                 case 1:
                     $mensaje= "Red 1";
-                    // return view('disponible');
-                    return view('red_6_cjpn');
+                    return view('disponible');
+                    // return view('red_6_cjpn');
                     break;
 
                 case 2:
                     $mensaje= "Red 2";
-                    // return view('disponible');
-                    return view('red_3_ej');
+                    return view('disponible');
+                    // return view('red_3_ej');
                     break;
 
                 case 3:
                     $mensaje= "Red 3";
-                    // return view('disponible');
+                    // return view('disponible'); activar
                     return view('red_7_sijpa');
                     break;
 
                 case 4:
                     $mensaje= "Red 4";
-                    // return view('disponible');
-                    return view('red_2_rejem');
+                    return view('disponible');
+                    // return view('red_2_rejem');
                     break;
 
                 case 5:
                     $mensaje= "Red 5";
-                    // return view('disponible');
+                    //return view('disponible'); activar
                     return view('red_5_masc');
                     break;
 
                 case 6:
                     $mensaje= "Red 6";
-                    // return view('disponible');
+                    // return view('disponible'); activar
                     return view('red_1_cecofam');
                     break;
 
                 case 7:
                     $mensaje= "Red 7";
-                    // return view('disponible');
+                    // return view('disponible'); activar
                     return view('red_4_jjocmed');
                     break;
 
                 case 9:
                     $mensaje= "Red 9";
-                    // return view('disponible');
+                    // return view('disponible'); activar
                     return view('red_8_aj');
                     break;
 
