2dd1953944abb4b758b2ed3a416a57999a27974a
Jair Pavon <jairo.p4195@gmail.com>
2022-11-24 09:23:47 -0600

    HabilitaConstancia

--- app/Http/Controllers/generaConstancia.php
+++ app/Http/Controllers/generaConstancia.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Codedge\Fpdf\fpdf\Fpdf;
+required('Codedge\Fpdf\fpdf\Fpdf.php');
 use App\Models\constanciasUsuarios;
 use Auth;
 use User;
