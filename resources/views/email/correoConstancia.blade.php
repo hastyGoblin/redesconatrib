eb039de7249236d634fd613699da09e0d16f5d96
Jair Pavon <jairo.p4195@gmail.com>
2022-11-25 09:46:01 -0600

    ManualUsuario-EnvioCorreoConstancia

--- resources/views/email/correoConstancia.blade.php
+++ resources/views/email/correoConstancia.blade.php
@@ -15,22 +15,22 @@
                 <div class="form-content">
                     <div class="form-items">
                         <h3>Apreciable {!! nl2br($info['nombre']) !!}</h3>
-                        <p style="text-align: justify">Nos complace informarle que su constancia ya se encuentra disponible para su descarga en el micrositio <b>{!! nl2br($info['red']) !!}</b> para su descarga.</p>
+                        <p style="text-align: justify">Nos complace informarle que su constancia ya se encuentra disponible para su descarga en el micrositio <b>{!! nl2br($info['red']) !!} <br></p>
                         <p style="text-align: justify">Pasos para la descarga: </p>
 
                         <p style="text-align: justify">
-                            1. Ingresa al link: http://redesconatrib.poderjudicialcdmx.gob.mx/login <br>
-                            2. Ingresa los datos con los que te registraste <br>
-                            3. Verifica que tu nombre sea el correcto <br>
-                            4. Da click en “Descarga constancia” <br>
-                            5. Guarda tu constancia en un lugar seguro, porque la plataforma solo permite una sola descarga.
+                            1. Ingresa al link: http://redesconatrib.poderjudicialcdmx.gob.mx/login<br>
+                            2. Ingresa los datos con los que te registraste previamente<br>
+                            3. Dar click en el botón Descarga Constancia, que encontrarás en la parte superior derecha<br>
+                            4. Verifica que tu nombre sea correcto<br>
+                            5. Da click en “Descarga constancia”<br>
+                            6. Guarda tu constancia en un lugar seguro, ya que la plataforma solo permite una sola emisión y descarga de la misma.
                         </p><br>
                         <p style="text-align: justify">A nombre del Comité Organizador, agradecemos su participación.</p>
                    </div>
 
                     <div class="form-sent">
                        <div class="info-holder">
-
                         </div>
                     </div>
                 </div>
